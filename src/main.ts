1f4e0336d4db17839afc9c4c123d55a33d588a27
Marcelo Siles <marcepsit27@gmail.com>
2025-09-18 21:13:11 -0400

    modificando el main.ts q tenia errores

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { provideServiceWorker } from '@angular/service-worker';
 import { isDevMode } from '@angular/core';
 bootstrapApplication(App, {
   providers: [
+    
     provideServiceWorker('ngsw-worker.js', { enabled: !isDevMode() }),
   ],
 });
